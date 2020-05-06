af52353938d68122673af45dca6c39451ea7128a
Melinda Golden <info@melindagolden.com>
2020-05-05 21:44:15 -0400

    add: ProfilePage to private routes

--- src/base/routes.tsx
+++ src/base/routes.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 import { LoginPage } from '../pages/login';
+import { ProfilePage } from '../pages/profile';
 
 export const publicRoutes = [
   {
-    path: '/login',
+    path: '/signin',
     component: LoginPage,
   },
   {
     path: '*',
-    component: () => <Redirect to="/login" />,
+    component: () => <Redirect to="/signin" />,
   },
 ];
 
-export const privateRoutes = [];
+export const privateRoutes = [
+  {
+    path: '/home',
+    component: ProfilePage,
+  },
+];
