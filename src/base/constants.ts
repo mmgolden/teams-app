b013afd2557e085e6c38f19f0d9054da9c7a0c9c
Melinda Golden <info@melindagolden.com>
2020-05-06 17:24:43 -0400

    add: Token keys to constants for localStorage

--- src/base/constants.ts
+++ src/base/constants.ts
@@ -1,5 +1,10 @@
 export const ERROR_MESSAGES = {
+  GENERIC: 'Something went wrong',
   EMAIL_INVALID: 'Please use a valid email',
   EMAIL_REQUIRED: 'Please enter email',
   PASSWORD_REQUIRED: 'Please enter password',
 };
+
+export const ACCESS_TOKEN_KEY = 'wethos-access-token';
+export const REFRESH_TOKEN_KEY = 'wethos-refresh-token';
+export const CURRENT_USER_KEY = 'wethos-current-user';
