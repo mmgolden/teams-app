17e2214e34173275065d7f393e44db7c10c41195
Melinda Golden <info@melindagolden.com>
2020-05-06 19:32:33 -0400

    add: Cached user to UserProvider

--- src/components/UserProvider.tsx
+++ src/components/UserProvider.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { User } from '../typings/user';
+import { getCurrentUser } from '../base/auth/getCurrentUser';
+
+interface Props {
+  isAuthenticated: boolean;
+}
 
 interface IUserContext {
   user?: User;
@@ -11,8 +16,18 @@ const UserContext = React.createContext<IUserContext>({
   setUser: () => {},
 });
 
-const UserProvider: React.FC = ({ children }) => {
-  const [user, setUser] = React.useState<User | undefined>(undefined);
+const getCachedUser = (isAuthenticated: boolean) => {
+  if (!isAuthenticated) {
+    return undefined;
+  }
+
+  return getCurrentUser();
+};
+
+const UserProvider: React.FC<Props> = ({ isAuthenticated, children }) => {
+  const cachedUser = getCachedUser(isAuthenticated);
+
+  const [user, setUser] = React.useState<User | undefined>(cachedUser);
 
   const value = React.useMemo(() => ({ user, setUser }), [user]);
 
