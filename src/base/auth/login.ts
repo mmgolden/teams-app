6c8467e933ebb04f4ef6424a2afb370508e06e69
Melinda Golden <info@melindagolden.com>
2020-05-06 19:33:31 -0400

    add: Expiration date to login function

--- src/base/auth/login.ts
+++ src/base/auth/login.ts
@@ -2,6 +2,7 @@ import {
   ACCESS_TOKEN_KEY,
   REFRESH_TOKEN_KEY,
   CURRENT_USER_KEY,
+  ACCESS_TOKEN_EXPIRATION,
 } from '../constants';
 import { User } from '../../typings/user';
 import { Authentication } from '../../typings/authentication';
@@ -12,9 +13,15 @@ interface Options {
 }
 
 export const login = ({ authentication, user }: Options) => {
+  const now = new Date();
+  const expirationDate = new Date(
+    now.getTime() + authentication.expires_in * 1000
+  );
+
   try {
     localStorage.setItem(ACCESS_TOKEN_KEY, authentication.access_token);
     localStorage.setItem(REFRESH_TOKEN_KEY, authentication.refresh_token);
+    localStorage.setItem(ACCESS_TOKEN_EXPIRATION, expirationDate.toISOString());
     localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
   } catch (error) {
     console.error(error);
