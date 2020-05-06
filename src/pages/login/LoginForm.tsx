9d3e4ab74989cfa580e98414176e6b85d8846427
Melinda Golden <info@melindagolden.com>
2020-05-05 23:01:23 -0400

    add: Token API call to LoginForm

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import styled from '../../base/styled';
 import * as Yup from 'yup';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikHelpers } from 'formik';
 import { PrimaryButton } from '../../components/buttons';
 import { TextField } from '../../components/form';
 import { ERROR_MESSAGES } from '../../base/constants';
+import axios from 'axios';
+import { Status } from '../../components/form/Status';
 
 interface FormValues {
-  email: string;
+  username: string;
   password: string;
 }
 
-const initialValues: FormValues = { email: '', password: '' };
+const initialValues: FormValues = { username: '', password: '' };
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string()
+  username: Yup.string()
     .email(ERROR_MESSAGES.EMAIL_INVALID)
     .required(ERROR_MESSAGES.EMAIL_REQUIRED),
   password: Yup.string().required(ERROR_MESSAGES.PASSWORD_REQUIRED),
 });
+
+const handleSubmit = async (
+  values: FormValues,
+  actions: FormikHelpers<FormValues>
+) => {
+  const { username, password } = values;
+
+  // Get token from API
+  try {
+    const authentication = await axios.post(
+      `${process.env.REACT_APP_TOKEN_URL}`,
+      {
+        username,
+        password,
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        grant_type: process.env.REACT_APP_GRANT_TYPE,
+        scope: process.env.REACT_APP_SCOPE,
+      }
+    );
+
+    console.log(authentication);
+  } catch (error) {
+    actions.setStatus('Something went wrong');
+  }
+};
 
 export const LoginForm: React.FC = () => {
   return (
@@ -26,28 +54,36 @@ export const LoginForm: React.FC = () => {
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
+        onSubmit={handleSubmit}
+        render={({ status, setStatus }) => {
+          return (
+            <>
+              {status && (
+                <Status
+                  status="Something went wrong"
+                  close={() => setStatus(undefined)}
+                />
+              )}
+              <Form>
+                <TextField
+                  label="Email"
+                  name="username"
+                  placeholder="Email address"
+                  autoFocus
+                  autoComplete="email"
+                />
+                <TextField
+                  type="password"
+                  label="Password"
+                  name="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <PrimaryButton type="submit">Sign in</PrimaryButton>
+              </Form>
+            </>
+          );
         }}
-        render={(formikBag) => (
-          <Form>
-            <TextField
-              label="Email"
-              name="email"
-              placeholder="Email address"
-              autoFocus
-              autoComplete="email"
-            />
-            <TextField
-              type="password"
-              label="Password"
-              name="password"
-              placeholder="Password"
-              autoComplete="current-password"
-            />
-            <PrimaryButton type="submit">Sign in</PrimaryButton>
-          </Form>
-        )}
       />
     </FormContainer>
   );
