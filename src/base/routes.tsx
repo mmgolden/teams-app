79f2781b09371b1ba8fe7ecdfe9a30fb92c865b4
Melinda Golden <info@melindagolden.com>
2020-05-06 20:42:25 -0400

    add: Routes to constants

--- src/base/routes.tsx
+++ src/base/routes.tsx
@@ -2,27 +2,28 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import { LoginPage } from '../pages/login';
 import { ProfilePage } from '../pages/profile';
+import { ROUTES } from './constants';
 
 export const publicRoutes = [
   {
-    path: '/signin',
+    path: ROUTES.SIGN_IN,
     exact: true,
     component: LoginPage,
   },
   {
     path: '*',
-    component: () => <Redirect to="/signin" />,
+    component: () => <Redirect to={ROUTES.SIGN_IN} />,
   },
 ];
 
 export const privateRoutes = [
   {
-    path: '/home',
+    path: ROUTES.HOME,
     exact: true,
     component: ProfilePage,
   },
   {
     path: '*',
-    component: () => <Redirect to="/home" />,
+    component: () => <Redirect to={ROUTES.HOME} />,
   },
 ];
