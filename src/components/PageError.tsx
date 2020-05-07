032b0153af2ae480023ba41416d747eb6d0e4915
Melinda Golden <info@melindagolden.com>
2020-05-06 23:04:17 -0400

    add: PageError

---
+++ src/components/PageError.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from '../base/styled';
+
+interface Props {
+  error: string;
+}
+
+export const PageError: React.FC<Props> = ({ error }) => {
+  return (
+    <PageErrorContainer>
+      <h1 className="error-title">{error}</h1>
+    </PageErrorContainer>
+  );
+};
+
+const PageErrorContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  .error-title {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.colors.primaryFont};
+    font-weight: 400;
+    margin: 0;
+  }
+`;
