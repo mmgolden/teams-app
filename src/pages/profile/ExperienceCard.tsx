d824f84537f57b5d32aeeeb2c017c56194ce0d24
Melinda Golden <info@melindagolden.com>
2020-05-07 21:53:05 -0400

    add: Styling to ExperienceCard

--- src/pages/profile/ExperienceCard.tsx
+++ src/pages/profile/ExperienceCard.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from '../../base/styled';
 import { Card } from '../../components/Card';
 import { Specialist } from '../../typings/specialist';
+import DOMPurify from 'dompurify';
+import { sanitizeUrl } from '@braintree/sanitize-url';
+import { Divider } from '../../components/Divider';
 
 interface Props {
   specialist: Specialist;
@@ -12,53 +15,113 @@ export const ExperienceCard: React.FC<Props> = ({ specialist }) => {
 
   return (
     <StyledExperienceCard>
-      <h2 className="experience-title">Experience</h2>
+      <h2 className="experience-card-title">Experience</h2>
       {experience.length > 0 ? (
         <>
           {experience.map((exp, index) => {
+            const { title, company, description, links } = exp;
+
             return (
-              <div key={`${exp.title}-${index}`}>
-                <h3>{exp.title}</h3>
-                {exp.company && <h4>{exp.company}</h4>}
-                {exp.description && (
-                  <p dangerouslySetInnerHTML={{ __html: exp.description }} />
+              <>
+                <div key={`${title}-${index}`}>
+                  <h3 className="experience-title">{title}</h3>
+                  {company && <h4 className="experience-company">{company}</h4>}
+                  {description && (
+                    <p
+                      className="experience-description"
+                      dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(description),
+                      }}
+                    />
+                  )}
+                  {links && links.length > 0 && (
+                    <ul className="experience-link-list">
+                      {links.map((link) => {
+                        return (
+                          <li key={link.title}>
+                            <a
+                              href={sanitizeUrl(link.url)}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {link.title}
+                            </a>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  )}
+                </div>
+                {index !== experience.length - 1 && (
+                  <div className="experience-spacer">
+                    <Divider />
+                  </div>
                 )}
-                {exp.links && exp.links.length > 0 && (
-                  <ul>
-                    {exp.links.map((link) => {
-                      return (
-                        <li key={link.url}>
-                          <a href={link.url}>{link.title}</a>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                )}
-              </div>
+              </>
             );
           })}
         </>
       ) : (
-        <p className="empty-text">
-          It looks like you don't have any experience yet. Get out there and
-          show your stuff!
-        </p>
+        <p className="empty-text">No experience found</p>
       )}
     </StyledExperienceCard>
   );
 };
 
 const StyledExperienceCard = styled(Card)`
-  .experience-title {
+  .experience-card-title {
     color: ${({ theme }) => theme.colors.primaryFont};
     font-weight: 500;
-    margin: 0 0 0.75rem 0;
-    font-size: 1.25rem;
+    margin: 0 0 1.5rem 0;
+    font-size: 1.5rem;
   }
 
   .empty-text {
     color: ${({ theme }) => theme.colors.subtleFont};
     font-size: 1rem;
     margin: 0;
+  }
+
+  .experience-title {
+    color: ${({ theme }) => theme.colors.primaryFont};
+    font-weight: 500;
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 2rem;
+  }
+
+  .experience-company {
+    color: ${({ theme }) => theme.colors.subtleFont};
+    font-weight: 400;
+    margin: 0;
+    font-size: 1rem;
+    line-height: 1.75rem;
+  }
+
+  .experience-description {
+    color: ${({ theme }) => theme.colors.primaryFont};
+    margin: 0;
+    font-size: 1rem;
+    line-height: 1.75rem;
+  }
+
+  .experience-link-list {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    a {
+      color: ${({ theme }) => theme.colors.primary};
+      text-decoration: none;
+      display: block;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .experience-spacer {
+    padding: 2rem 0;
   }
 `;
