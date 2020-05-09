d93623f80ee4b38031dae4d5c962dc307a2661f2
Melinda Golden <info@melindagolden.com>
2020-05-09 17:29:53 -0400

    add: Specialist, organization, and project factories for testing

--- src/__testUtils__/factories.ts
+++ src/__testUtils__/factories.ts
@@ -1,4 +1,6 @@
 import { User } from '../typings/user';
+import { Specialist } from '../typings/specialist';
+import { Project } from '../typings/project';
 
 export const userFactory = (values?: { [key: string]: string }): User => ({
   id: 1,
@@ -10,5 +12,79 @@ export const userFactory = (values?: { [key: string]: string }): User => ({
   checkpoints: [],
   phone_number: null,
   roles: [],
+  ...values,
+});
+
+export const organizationFactory = (values?: { [key: string]: string }) => ({
+  about: null,
+  description: null,
+  id: 1,
+  name: 'Dunder Mifflin',
+  notes: null,
+  overview: null,
+  slug: 'dunder-mifflin',
+  tldr: null,
+  website_url: null,
+  ...values,
+});
+
+export const projectFactory = (values?: {
+  [key: string]: string;
+}): Project => ({
+  accountstrategist: userFactory(),
+  brief: null,
+  checkpoints: [],
+  contacts: [],
+  created_at: '2020-05-04 17:09:43',
+  currentscope: null,
+  description: 'Ratione a velit quod voluptatum.',
+  emoji: null,
+  id: 1,
+  links: [],
+  milestones: [],
+  name: 'Schrute Farms',
+  notes: null,
+  organization: organizationFactory(),
+  payment_model: 'full',
+  scopes: [],
+  slack_channel: null,
+  slug: 'schrute-farms',
+  sow_template_organization: null,
+  sow_template_specialist: null,
+  specialists: [],
+  statement_of_purpose: 'Possimus est ea cupiditate facilis.',
+  status: 'sales',
+  tags: [],
+  teamlead: userFactory(),
+  ...values,
+});
+
+export const specialistFactory = (values?: {
+  [key: string]: string;
+}): Specialist => ({
+  assets: [],
+  balance: 0,
+  causes: [],
+  checkpoints: [],
+  deliverables: [],
+  description: '',
+  experience: [],
+  has_transactions: 0,
+  id: 1,
+  languages: [],
+  links: [],
+  next_payment_at: null,
+  notes: null,
+  payable: false,
+  paymentrails_widget_url: '',
+  profile_image: '',
+  projects: [{ ...projectFactory() }],
+  raw: '',
+  slug: null,
+  title: '',
+  tldr: null,
+  user: userFactory(),
+  city: null,
+  tags: [],
   ...values,
 });
