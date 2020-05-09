724350cebc78b2b83e44ed2ce53c3bfb60e49f75
Melinda Golden <info@melindagolden.com>
2020-05-09 10:11:37 -0400

    add: Cards to ProjectPage

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -10,6 +10,12 @@ import {
   TOKEN_TYPE,
   ERROR_MESSAGES,
 } from '../../base/constants';
+import { ProjectDetailsCard } from './ProjectDetailsCard';
+import { ContactCard } from './ContactCard';
+import { ProjectLeadCard } from './ProjectLeadCard';
+import { AccountStrategistCard } from './AccountStrategistCard';
+import { SpecialistsCard } from './SpecialistsCard';
+import { LinksCard } from './LinksCard';
 
 export const ProjectPage: React.FC = () => {
   const { projectId } = useParams<{ projectId: string }>();
@@ -61,10 +67,29 @@ export const ProjectPage: React.FC = () => {
   return (
     <Layout>
       <ProjectPageContainer>
-        <h1>hello</h1>
+        <ProjectDetailsCard project={project} />
+        <div className="project-column">
+          <ContactCard project={project} />
+          <ProjectLeadCard project={project} />
+          <AccountStrategistCard project={project} />
+          <SpecialistsCard project={project} />
+          <LinksCard project={project} />
+        </div>
       </ProjectPageContainer>
     </Layout>
   );
 };
 
-const ProjectPageContainer = styled.div``;
+const ProjectPageContainer = styled.div`
+  padding: 2rem 0;
+
+  @media screen and (min-width: 992px) {
+    display: flex;
+  }
+
+  .project-column {
+    @media screen and (min-width: 992px) {
+      flex: 1;
+    }
+  }
+`;
